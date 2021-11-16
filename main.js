65f922cf8500142364fe5eec6750736f14395765
mirandanilhag <miranda_nilhag@hotmail.com>
2021-11-16 10:07:25 +0100

    commit

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ function startGame() {
   state = {};
   showTextNode(1);
 }
+/* ser till att vi hamnar på rätt fråga baserat på vilket val man gör */
+/* while/for each loop för att ta bort alternativen och ha olika alternativ beroende på vilket state man är i */ 
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
+  storyElement.innerText = textNode.text;
+  while (choiceButtonsElement.firstChild) {
+    choiceButtonsElement.removeChild(choiceButtonsElement.firstChild);
+  }
+
+  textNode.choice.forEach((choice) => {
+    if (showChoice(choice)) {
+      const button = document.createElement("button");
+      button.innerText = choice.text;
+      button.classList.add("btn");
+      button.addEventListener("click", () => selectChoice(choice));
+      choiceButtonsElement.appendChild(button);
+    }
+  });
+}
